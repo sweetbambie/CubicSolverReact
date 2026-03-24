92d78aa5f2bffdd0f3c3119d9123a2d8eb3629e8
Sophia Miao <sophia@miao.fm>
2026-03-23 23:53:37 -0400

    added the equation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
 import { CubicInput } from "./component/CubicInput";
+import { CubicEquation } from "./component/CubicEquation";
 
 export const App = () => {
-  return <CubicInput onSave={(a, b, c, d) => console.log(a, b, c, d)} />;
+  const [coefficients, setCoefficients] = useState({ a: 0, b: 0, c: 0, d: 0 });
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "20px", padding: "20px" }}>
+      <CubicInput onSave={(a, b, c, d) => setCoefficients({ a, b, c, d })} />
+      <CubicEquation {...coefficients} />
+    </div>
+  );
 };
