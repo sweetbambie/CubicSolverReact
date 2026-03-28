bcfc05fec290d103595b76d0c313f3725d3d5ca3
Sophia Miao <sophia@miao.fm>
2026-03-28 13:56:15 -0400

    fixed format

--- src/component/CubicEquation.tsx
+++ src/component/CubicEquation.tsx
@@ -7,21 +7,8 @@ interface CubicEquationProps {
 
 export const CubicEquation = ({ a, b, c, d }: CubicEquationProps) => {
   return (
-    <div style={{
-      width: "400px",
-      padding: "20px",
-      background: "rgb(255, 189, 226)",
-      borderRadius: "8px",
-      fontFamily: "'Courier New', Courier, monospace",
-    }}>
-      <h1 style={{
-        color: "rgb(255, 0, 144)",
-        marginBottom: "16px",
-        fontSize: "28px",
-        fontWeight: "bold",
-        borderBottom: "2px solid rgb(245, 140, 199)",
-        paddingBottom: "10px",
-      }}>
+    <div style={{ width: "400px", padding: "20px", background: "rgb(255, 189, 226)", borderRadius: "8px", fontFamily: "'Courier New', Courier, monospace",}}>
+      <h1 style={{ color: "rgb(255, 0, 144)", marginBottom: "16px", fontSize: "28px",fontWeight: "bold", borderBottom: "2px solid rgb(245, 140, 199)", paddingBottom: "10px",}}>
         Equation
       </h1>
       <p style={{ color: "rgb(255, 0, 144)", fontSize: "18px" }}>
