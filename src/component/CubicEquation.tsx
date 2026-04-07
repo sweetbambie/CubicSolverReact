8185b1776eb8828df616eb05de2ad13dddffe758
Sophia Miao <sophia@miao.fm>
2026-04-06 20:47:39 -0400

    forgot the font lol

--- src/component/CubicEquation.tsx
+++ src/component/CubicEquation.tsx
@@ -7,7 +7,7 @@ interface CubicEquationProps {
 
 export const CubicEquation = ({ a, b, c, d }: CubicEquationProps) => {
   return (
-  <div className="w-[400px] p-[20px] bg-[rgb(255,189,226)] rounded-[8px] text-[rgb(255,0,144)]">
+  <div className="w-[400px] p-[20px] bg-[rgb(255,189,226)] rounded-[8px] text-[rgb(255,0,144)] font-['Courier_New',Courier,monospace]">
     <h1 className="mb-4 text-[28px] font-bold border-b-2 border-[rgb(245,140,199)] pb-[10px]">
       Equation
     </h1>
--- src/component/CubicInput.tsx
+++ src/component/CubicInput.tsx
@@ -15,7 +15,7 @@ export const CubicInput = ({ onChange, onSave }: CubicInputProps) => {
     onSave(a, b, c, d);
   };
   return (
-  <div className="w-[400px] p-[20px] bg-[rgb(255,189,226)] rounded-[8px] text-[rgb(255,0,144)]">
+  <div className="w-[400px] p-[20px] bg-[rgb(255,189,226)] rounded-[8px] text-[rgb(255,0,144)] font-['Courier_New',Courier,monospace]">
     <h1 className="mb-4 text-[28px] font-bold border-b-2 border-[rgb(245,140,199)] pb-[10px]">
       Cubic Solver
     </h1>
